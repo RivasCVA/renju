a816f241421f49d97bccc40d5c61d2d783ec06ba
Carlos Rivas <rivascarlosva@gmail.com>
2021-03-07 22:00:40 -0600

    Minor GamePage refactor

--- renju-app/src/pages/GamePage/GamePage.tsx
+++ renju-app/src/pages/GamePage/GamePage.tsx
@@ -8,6 +8,7 @@ import './GamePage.css';
 /** The main game page. All live game-related content will begin here. */
 export const GamePage = (): JSX.Element => {
   const history = useHistory();
+  const INTERSECTION_DIST = 37;
   const [pieces, setPieces] = useState<JSX.Element[]>([]);
 
   const goHome = () => {
@@ -16,7 +17,10 @@ export const GamePage = (): JSX.Element => {
 
   // Rounds the given X and Y position to the nearest board intersection
   const roundPositionToCorner = (posX: number, posY: number) => {
-    return [37 * Math.round(posX / 37), 37 * Math.round(posY / 37)];
+    return [
+      INTERSECTION_DIST * Math.round(posX / INTERSECTION_DIST),
+      INTERSECTION_DIST * Math.round(posY / INTERSECTION_DIST),
+    ];
   };
 
   const handleBoardClick = (posX: number, posY: number) => {
